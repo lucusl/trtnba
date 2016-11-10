51cd933618da2a428869e0e253837a7952b9519c
lucusl <lucus.lovell@gmail.com>
2016-11-10 20:14:35 +1100

    more dev work

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,130 +9,89 @@
  */
 angular.module('trtNbaApp')
   .controller('MainCtrl', function (data) {
-
-
     
   //sets the controller alias 
-    var main = this;
+  var main = this;
 
-  function Team(teamName,gameId,teamNum,ownerName){
+  function Team(teamName,gameId,teamNum){
      this.name = teamName;
      this.gameId = gameId; // 0 - 1 
      this.teamId = 'Team' + teamNum; //
      this.teamStyle = [];
      this.owner = {};
-     this.owner.name = ownerName;
+     this.owner.name = [];
      this.owner.details = {};
    }
 
    main.teamOne = new Team('not set','0', '1');
+   main.teamTwo = new Team('not set','0', '2');
+   main.teamThree = new Team('not set','1', '1');
+   main.teamFour = new Team('not set','1', '2');
+
+
    console.log(main.teamOne);
    
         main.data ={};
-        main.data.result = 'none yet'
+        main.data.result = 'none yet';
         main.gameTime =[];
         main.apiError = [];
-        main.teamOneStyle = [];
-        main.teamTwoStyle = [];
-        main.apiError = [];
-
-  //Team 1
-    main.team1 = {};
-    main.team1.name = 'Team 1 not set';
-    main.team1.owner = {};
-    main.team1.owner.name = 'owner name not set';
-	main.team1.owner.details ='owner details not set';
-	
-  //Team 2
-    main.team2 = {};
-    main.team2.name = 'Team 2 not set';
-    main.team2.owner = {};
-    main.team2.owner.name = 'owner name not set';
-    main.team2.owner.details = {};
-  
-
 
   // Calls the service and deals with the data 
   data.getData()
     	.then(function(result){
       		main.data = JSON.parse(result) ; 
     		console.log(main.data);
-    		gameDay(main.data);
+
+    		//check data to see if it is game day
+    		isGameDay(main.data);
+    		//check data to see number of games 
     		gameNum(main.data);
-    		main.teamOne.getTeam(main.data);
 
+    		main.teamOne.getTeamDeets(main.data);
+    		main.teamTwo.getTeamDeets(main.data);
+    		main.teamThree.getTeamDeets(main.data);
+    		main.teamFour.getTeamDeets(main.data);
 
-    		main.team1.name = getTeam1();
-    		main.teamOneStyle = setTeam(main.team1.name, 1);
-     		console.log(main.teamOne.getTeam());
-
-        main.team1.owner.name = getOwner1();
-        main.team1.owner.details  = getOwnerDeets(main.team1.owner.name);
-
-    		main.team2.name = getTeam2();
-    		main.teamTwoStyle = setTeam(main.team2.name, 2);
-    		console.log(main.teamTwoStyle)
-
-    		main.team2.owner.name = getOwner2();
-    		main.team2.owner.details = getOwnerDeets(main.team2.owner.name);
 
     });
 
     console.log(main.data);
 
-	 Team.prototype.getTeam = function(gameData){
+	 Team.prototype.getTeamDeets = function(gameData){
       var a = this.gameId;
       var b = this.teamId;
       var game = gameData.Games[a];
-      console.log(game);
       this.name = game[b].TeamFullName;
       this.owner = game[b].Owners;
       this.getOwner();
-	}
+      this.teamStyle = setTeam(this.name);
+      console.log(this.teamStyle);
+	};
 
 	 Team.prototype.getOwner = function(){
 	      if (this.owner.length === 2){
-	      	   return {this.owner.name: this.owner[0].DisplayName +' & '+ this.owner[1].DisplayName, this.owner.intro:'Team owners'}
+	      	   this.owner.name = this.owner[0].DisplayName +' & '+ this.owner[1].DisplayName;
+	      	   this.owner.intro ='Team owners';
   		} else {
-               return {this.owner.name: this.owner[0].DisplayName, intro:'Team owner'}
+               this.owner.name = this.owner[0].DisplayName;
+               this.owner.intro ='Team owner';
 	      }
-	}
+	};
+
 
 	function gameNum(result){
 		if (main.data.Games.length === 1) {
-			console.log('only one game today')
+			console.log('only one game today');
 		}else if(main.data.Games.length === 2){
-			console.log('2 games today')
+			console.log('2 games today');
 		}else{
-			console.log('probs no game')
+			console.log('probs no game');
 		}
 
 	}
 
-   	function getTeam1() {
-  		return main.data.Games[0].Team1.TeamFullName
-  	}
-	  function getTeam2() {
-  		return main.data.Games[0].Team2.TeamFullName
-  	}
-  	function getOwner1() {
-  		return main.data.Games[0].Team1.Owners
-  	}
-  	function getOwner2() {
-  		return main.data.Games[0].Team2.Owners
-  	}
-
-  	function getOwnerDeets(owner) {
-  		if (owner.length === 2) {
-         return {name: owner[0].DisplayName +' & '+ owner[1].DisplayName, intro:'Team owners'}
-  		} else {
-         return {name: owner[0].DisplayName, intro:'Team owner'}
-  		}
-  	}
-
-
     // Checks to see if there is game data for the day & if it is due to API down 
-  	function gameDay(gameData){
+  	function isGameDay(gameData){
   		if (gameData === null){
   			console.log('no games today');
         main.gameTime = false;
@@ -152,7 +111,7 @@ angular.module('trtNbaApp')
   }
 
     // Sets the style and team logo based on team name data  
-  	function setTeam(TeamName, TeamID){
+  	function setTeam(TeamName){
   		switch(TeamName)
   		{
   			case "Portland Trail Blazers":
@@ -173,16 +132,16 @@ angular.module('trtNbaApp')
   			case "Brooklyn Nets":
   				return {style:"nets", logo:"images/bn.png"}; 
   			break;
-  			case "New York Nicks":
+  			case "New York Knicks":
   				return {style:"nicks", logo:"images/nyn.png"}; 
   			break;
   			case "Memphis Grizzlies":
   				return {style:"grizzlies", logo:"images/griz.png"}; 
   			break;
-          break;
-     	   case "Golden State Warriors":
-     	     return {style:"Warriors", logo:"images/gs.png"}; 
-     	   break;
+	        break;
+	     	   case "Golden State Warriors":
+	     	     return {style:"Warriors", logo:"images/gs.png"}; 
+	     	break;
   			default: 
   		}
 
--- app/scripts/services/data.js
+++ app/scripts/services/data.js
@@ -18,14 +18,13 @@ angular.module('trtNbaApp')
 
  // get todats date in required format 
  	function getDay() {
-  		var timestmp = 
-  		     new Date().setFullYear(new Date().getFullYear(), 0, 1);
+  		var timestmp = new Date().setFullYear(new Date().getFullYear(), 0, 1);
   		var yearFirstDay = Math.floor(timestmp / 86400000);
   		var today = Math.ceil((new Date().getTime()) / 86400000);
   		var dayOfYear = today - yearFirstDay;
 
     console.log(dayOfYear)
-  		return dayOfYear + 2;
+  		return dayOfYear;
   	}
 
   	function extract(result){
